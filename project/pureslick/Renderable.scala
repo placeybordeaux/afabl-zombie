a9aca397f0c10f0a42bcba0478222d1bd5c8fe79
Peter Lacey-Bordeaux <placeybordeaux@gmail.com>
2012-06-28 14:18:08 -0400

    'camera' now follows the player

---
+++ project/pureslick/Renderable.scala
@@ -0,0 +1,13 @@
+package pureslick
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: peter
+ * Date: 6/26/12
+ * Time: 11:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+trait Renderable {
+
+}
