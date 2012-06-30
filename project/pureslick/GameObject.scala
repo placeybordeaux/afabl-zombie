4b7dd664090b070e2e89af372c4f670d95587109
Peter Lacey-Bordeaux <placeybordeaux@gmail.com>
2012-06-30 16:15:52 -0400

    added some abstraction, zombies chase and die

---
+++ project/pureslick/GameObject.scala
@@ -0,0 +1,22 @@
+package pureslick
+
+import org.jbox2d.dynamics.Body
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: peter
+ * Date: 6/30/12
+ * Time: 4:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+trait GameObject extends Renderable{
+  var isGarbage = false
+  val body: Body
+
+  def update = {}
+
+  def update(observation: Observation): Unit = {update}
+
+
+}
